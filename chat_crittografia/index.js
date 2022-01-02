320c766a123db70b236b8dcab6e9adf74f0a25a7
V1to2 <67048779+V1to2@users.noreply.github.com>
2022-01-02 14:14:37 +0100

    Mod_02

--- chat_crittografia/index.js
+++ chat_crittografia/index.js
@@ -6,23 +6,32 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 const { uuid } = require('uuidv4');
 
-var stanza = "hih";
-app.use(express.static('public'))
+// Pacchetti importati 
 
-io.on('connection', (socket) => {
-  
-  socket.join(stanza)
-  console.log("UN utente si è connesso: " + socket.id)
-  io.to(stanza).emit('connection', socket.id, stanza )
+var stanza = "hih";
+app.use(express.static('public')) // Indicazione dir per express
+
+io.on('connection', (socket) => { // Quando un socket accede/ si connette a localhost, questa funzione verra eseguita
+
+  socket.join(stanza) // join attraverso id della stanza
+  console.log("UN utente si è connesso: " + socket.id) // log di chi si e connesso attraverso il suo id su terminale
+  io.to(stanza).emit('connection', socket.id, stanza) // Viene inviata una "notifica" a tutti gli altri cliente connessi nella stanza
 
   // Gestione invio messaggi
-  socket.on('chat message', (msg) => {
-    io.to(stanza).emit('chat message', msg, socket.id);
-    io.to(stanza).emit('user',  socket.id);
+  socket.on('chat message', (msg) => { // Quando viene crato un messaggio viene avviata questa funzione
+    
+
+
+
+
+
+
+    io.to(stanza).emit('chat message', msg, socket.id); // Viene emessa un'altra notifica con il messaggio e chi l'ha inviato
+    // io.to(stanza).emit('user',  socket.id);
   });
 
   // Gestione disconnessione da stanza
-  socket.on('disconnect', () => {
+  socket.on('disconnect', () => { // Gestione disconnessione di un utente
     console.log('user disconnected');
   });
 
@@ -36,7 +45,7 @@ io.on('connection', (socket) => {
   });
 
   socket
-  
+
 });
 server.listen(3000, () => {
   console.log('listening on *:3000');
